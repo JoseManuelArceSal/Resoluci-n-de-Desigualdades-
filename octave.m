04b726c9ece32ad9bd1bf4652ac57cdaf189d49f
Jose Manuel Arce <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-10-07 22:43:29 -0500

    Add files via upload

---
+++ octave.m
@@ -0,0 +1,20 @@
+%octave script
+%Title        :Inecuaciones en octave
+%Descripcion  :Resuelva inecuaciones y grafique
+%Autor        :Jose Manuel Arce Salvador
+%Date         :202123801
+%version      :1
+%Usage        :Octave
+
+"ineciaciones  de primer Grado"
+syms x
+solve (7x+5<2x-10)
+solve  (-6*x+9>0)
+solve  (x*2 +3*x +3 >0)
+
+"inecuaciones de segundo Grado"
+solve (x^2-6*x-7>0)
+solve (3*x^3-x<3*x-1)
+"innecuaciones valor absoluto"
+abs (x+7)
+abs  (5*x+6)
