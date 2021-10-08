7df28f7fb1f37e6ca71c9e2e85c8cfda59478b7c
Jose Manuel Arce <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-10-07 23:17:22 -0500

    Update octave.m
    Actualización con los cambios del equipo

--- octave.m
+++ octave.m
@@ -9,12 +9,20 @@
 "ineciaciones  de primer Grado"
 syms x
 solve (7x+5<2x-10)
-solve  (-6*x+9>0)
-solve  (x*2 +3*x +3 >0)
-
+solve (7x-2x<-10-5) 
+solve  5x<-15
+solve x<-15/5
+solve x<-3
 "inecuaciones de segundo Grado"
-solve (x^2-6*x-7>0)
-solve (3*x^3-x<3*x-1)
+solve (2x^2=50)
+solve (x^2=3x)
+solve (5x^2-2x+1=0)
+solve (7x-x^2=0)
+solve (3x*x-5*=6x)
 "innecuaciones valor absoluto"
-abs (x+7)
-abs  (5*x+6)
+abs (|3x-2|<1)
+abs  (-1<3x-2|<1)
+abs  ( -3<3x<1)
+abs  (-1<x<0.5)
+subplot ( -1,0.5)
+plot (x,y)
